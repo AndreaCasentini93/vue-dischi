6a01b5a159dfe28ae9aa140e3b738a9367daad1f
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-06-10 12:53:47 +0200

    Image Refactoring

--- src/components/Disc.vue
+++ src/components/Disc.vue
@@ -33,7 +33,6 @@ export default {
         img {
             display: block;
             width: 100%;
-            height: 150px;
             margin-bottom: 15px;
             object-fit: cover;
             object-position: top;
